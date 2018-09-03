3de245924a7f65bd4ffb453b4ebce749a451e240
zaxs1320 <42888702+zaxs1320@users.noreply.github.com>
2018-09-03 12:35:06 +0800

    Add files via upload

---
+++ data/post-data.js
@@ -0,0 +1,124 @@
+
+var local_data = [
+  {
+    name: '铃',
+    context: '我想知道，我们是否真的能够互相懂得。不是包容、不是照看、也不是原谅或宠爱，而是懂得，像解一道数学题那样，经历曲折和明暗去明白一个人的心',
+    data: 'National Day the 2nd',
+    text1: '520',
+    text2: '1314',
+
+    image0: 'https://mmbiz.qpic.cn/mmbiz_jpg/mqGoDBKZBzO2Yf06Nx5TSuzMpstZ7g5iaN0ctdRaKBnAy2kOz1BcG8fwk0Sb2VG9eWL4PSO9QuhLZ85sZPSdsOQ/0?wx_fmt=jpeg',
+    image1: '/image/7.png',
+    image2: '/image/8.png',
+    postId: 0,
+
+    author: '铃',
+    time: '五月二日零点十三分十四秒',
+    toptext: '愚蠢的智商 不适合想太多',
+    textcontent: '有人说\n当你找到喜欢的新歌时\n便不会再那么喜欢过去那些曾经痴迷的歌曲\n但假如有一天不小心看到了我的歌单\n你会一直听我的歌单循环到死\n所以并不是喜新厌旧\n只是没有看到我的歌单而已\n歌是  你也是  我也一样',
+    bottomauthor: '———雨霖铃',
+
+    music: {
+      url: "https://res.wx.qq.com/voice/getvoice?mediaid=MzU1OTE0MjE5M18xMDAwMDAwMjI=",
+      title: "'玲'珑骰子安红豆",
+    }
+
+  },
+  {
+    name: '多吃青菜',
+    context: '下次有事找我直接说事，不要问我在不在，你让我怎么回答你，我要说我在你找我借钱咋整，我要说不在你请我吃饭咋整，我要说在找我没好事咋整，我要不在有好事落下咋整，所以再找我直接说事，我好知道我在不在',
+    data: 'February the 2nd',
+    text1: '100',
+    text2: '105',
+
+    image0: 'https://mmbiz.qpic.cn/mmbiz_jpg/mqGoDBKZBzO2Yf06Nx5TSuzMpstZ7g5iankibWib3Rz8ibIiawAS8EnnsVjKRK72eG5pnFUFcDnbjyBtBWIaaBIW3Bw/0?wx_fmt=jpeg',
+    image1: '/image/7.png',
+    image2: '/image/8.png',
+    postId: 1,
+
+    author:'匆匆',
+    time:'三天前',
+    toptext: '越快放弃旧奶酪  越早享受新奶酪',
+    textcontent:'如果不再恐惧 你会做什么??\n只有相信自己能找到新奶酪 你才会改变自己的行为\n改变自己的最快速的方法  就是嘲笑自己的愚蠢',
+    bottomauthor: '———陈瑜',
+
+    music:{
+      url: "http://res.wx.qq.com/voice/getvoice?mediaid=MzAwMDk5MTg1MF8yMjQ3NDg0NTI4",
+      title: "请给我很多很多的爱",
+    }
+    
+  },
+  {
+    name: '多吃白菜',
+    context: '童话里的浪漫 需要用心去培养',
+    data: 'February the 3nd',
+    text1: '58',
+    text2: '92',
+
+    image0: 'https://mmbiz.qpic.cn/mmbiz_jpg/mqGoDBKZBzO2Yf06Nx5TSuzMpstZ7g5iaMgf4kTPiaAuH0f6wooYnAm2QzDOsXB9xuF5sfxhvR3XxCibiaMFoVOzwg/0?wx_fmt=jpeg',
+    image1: '/image/7.png',
+    image2: '/image/8.png',
+    postId: 2,
+
+    author: '噗噗',
+    time: '一天前',
+    toptext: '愿有人陪你颠沛流离',
+    textcontent: '曾经很惧怕孤独\n一个人的时候\n会莫名的烦躁不安\n无所事事度过一整天\n什么也做不了\n后来\n有一个人对我说\n如果你想去实现梦想\n孤独 是你的必修课\n如果不能沉下心来\n就没有办法去实现它\n因为 那绝对不是一件容易的事情 \n孤独 能让你更坚强\n你必须找到自己的生活节奏',
+    bottomauthor: '———刘刚',
+
+    music: {
+      url: "https://res.wx.qq.com/voice/getvoice?mediaid=MzU1OTE0MjE5M18xMDAwMDAwMDE=",
+      title: "分身情人",
+    }
+  },
+  {
+    name: '丧',
+    context: '平时总笑人家女儿态  如今识得愁滋味',
+    data: '2018-05-06 0.59',
+    text1: '23',
+    text2: '33',
+
+    image0: 'https://mmbiz.qpic.cn/mmbiz_jpg/mqGoDBKZBzO2Yf06Nx5TSuzMpstZ7g5iaPALu9D909xc3BluZO79ascP5KCyhwu86cVd2JryDfwekLjjs6Iic0xQ/0?wx_fmt=jpeg',
+    image1: '/image/7.png',
+    image2: '/image/8.png',
+    postId: 3,
+
+    author: '聪聪',
+    time: 'time:5月6日 凌晨一点',
+    toptext: '过去种种 譬如昨日死 以后种种 譬如今日生',
+    textcontent: '想说些什么 却没什么好说的/n或许代码会更懂我吧',
+    bottomauthor: '———聪聪',
+
+    music: {
+      url: "https://res.wx.qq.com/voice/getvoice?mediaid=MzU1OTE0MjE5M18xMDAwMDAwMjM=",
+      title: "take me hand",
+    }
+  },
+  {
+    name: '多吃林/玲同学',
+    context: '一个人的美丽是认真 两个人的认真是爱情',
+    data: 'February the 5nd',
+    text1: '322',
+    text2: '211',
+
+    image0: 'https://mmbiz.qpic.cn/mmbiz_jpg/mqGoDBKZBzO2Yf06Nx5TSuzMpstZ7g5iaGZqCBesaib4EpLa1LVdiacUJUHssT49AiboIZiaH0oHmy4ibSp8FNacE9bg/0?wx_fmt=jpeg',
+    image1: '/image/7.png',
+    image2: '/image/8.png',
+    postId: 4,
+
+    author: '菜菜',
+    time: '一天前',
+    toptext: 'No matter who you are, I just want you',
+    textcontent: '有段时间很奇怪\n迫切的想要变坏\n似乎觉得变坏了才能找到自己想要的自由\n熬夜通宵的频率越来越高\n偶尔也打打游戏\n自己都觉得自己没救了\n我听过很多人跟我说熬夜不好\n至今依然感谢他们的善意\n可我啊 又倔又坏 就是不想改\n后来我把手机里所有音乐软件都删掉了\n有些时候会突然想起一个女孩\n她说我听的歌总是不快乐的\n她是第一个告诉我哪种咖啡加橙子对熬夜更合适一些\n后来我把她弄丢了 没有听她的话去选择\n我习惯了喝她推荐的咖啡 味道其实真的很烂',
+    bottomauthor: '———夜晚跑丢了的猴子',
+
+    music: {
+      url: "https://res.wx.qq.com/voice/getvoice?mediaid=MzU1OTE0MjE5M18xMDAwMDAwMDI=",
+      title: "你要好  不许不好",
+    }
+  },
+]
+
+module.exports={
+    postlist:local_data
+}
